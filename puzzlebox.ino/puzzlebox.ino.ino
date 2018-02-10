c4b351118f3bfd1dae63bf25db7ce01e06f86c83
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2018-02-10 22:56:40 +0100

    print a string with potentiometer

--- puzzlebox.ino/puzzlebox.ino.ino
+++ puzzlebox.ino/puzzlebox.ino.ino
@@ -1,31 +1,36 @@
 #include <LiquidCrystal.h>
 
-// LCD screen pins 
-const int rs = 12, 
-         en = 11, 
-         d4 = 5, 
-         d5 = 4, 
-         d6 = 3, 
-         d7 = 2; 
-         
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
+// LCD screen pins
+const int rs = 12,
+          en = 11,
+          d4 = 5,
+          d5 = 4,
+          d6 = 3,
+          d7 = 2;
 
-void setup() 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const char *lcdTop[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+const char *lcdBottom[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+void setup()
 {
-    Serial.begin(9600);  
-    lcd.begin(16,2);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Text d'intro");
+  delay(3000);
+  lcd.clear();
 }
 
 void loop()
 {
-   int PotOne = map(analogRead(A0), 0, 1023, 0, 9); 
-   int PotTwo = map(analogRead(A1), 0, 1023, 0, 9); 
-   int PotThree = map(analogRead(A2), 0, 1023, 0, 9); 
-   
-   lcd.setCursor(0, 0); 
-   lcd.print(PotOne); 
-   lcd.setCursor(2, 0); 
-   lcd.print(PotTwo); 
-   lcd.setCursor(4, 0); 
-   lcd.print(PotThree); 
+  // ---- POTENTIOMETER ----
+  int potOne = map(analogRead(A0), 0, 1023, 0, 9);
+  int potTwo = map(analogRead(A1), 0, 1023, 0, 9);
+
+  lcd.setCursor(0, 0);
+  lcd.print(lcdTop[potOne]);
+  lcd.setCursor(0, 1);
+  lcd.print(lcdBottom[potTwo]);
 }
